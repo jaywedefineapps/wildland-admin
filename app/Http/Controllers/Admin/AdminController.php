58c3c4b9164b24fcc605f4527342a1ee9114c46e
almightyphp <almighty.php.dev1607@gmail.com>
2024-06-15 12:18:25 +0530

    dumping the variables

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -20,15 +20,21 @@ class AdminController extends Controller
             'email' => $request->email,
             'password' => $request->password,
         ])) {
+            dump('login done');
             $role_id = auth()->guard('admin')->user()->role_id;
+            dump('role id');
+            dump($role_id);
             $data['roleAccesses'] = RoleAccess::select('type')
                 ->join('accesses', 'accesses.id', 'role_accesses.access_id')
                 ->where('role_id', $role_id)
                 ->pluck('type')
                 ->toArray();
-            Session::put(['roleAccesses' => $data['roleAccesses']]);
+            dump($data['roleAccesses']);
+            dd(Session::put(['roleAccesses' => $data['roleAccesses']]));
+            
             return redirect()->route('admin.dashboard')->with('success', 'Admin Login Successfully');
         } else {
+            dd('not login');
             return back()->with('error', 'Invalid Email Or Password');
         }
     }
