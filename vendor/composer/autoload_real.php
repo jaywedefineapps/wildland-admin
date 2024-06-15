5e17e49c838ca16cd994a280338099985a2f546c
ksojitra747 <ksojitra747@gmail.com>
2024-06-15 11:04:34 +0530

    Updated composer.json

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInite813bf851987969d66fd68874188561c
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInite813bf851987969d66fd68874188561c', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInite813bf851987969d66fd68874188561c', 'loadClassLoader'));
