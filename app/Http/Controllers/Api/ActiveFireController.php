37c25c1f76ec124f304845b6b40b5b05796c733a
almightyphp <almighty.php.dev1607@gmail.com>
2024-06-19 10:19:07 +0530

    active fire api changes

--- app/Http/Controllers/Api/ActiveFireController.php
+++ app/Http/Controllers/Api/ActiveFireController.php
@@ -56,6 +56,42 @@ class ActiveFireController extends Controller
                 $this->activeFireService->createViirsSnppNrt($value);
             }
         }
+        $responseViirsSnppNrt = Http::get('https://firms.modaps.eosdis.nasa.gov/api/area/csv/5cd8bf082b0eb8dbc40701e0633c65b5/VIIRS_NOAA21_NRT/world/1/'.$currentDate ); //2024-06-01');
+        $csvData = $responseViirsSnppNrt->body();
+        if(!empty($csvData)){
+            $rows = explode("\n", trim($csvData));
+            $header = str_getcsv(array_shift($rows));
+            $data = array();
+
+            foreach ($rows as $row) {
+                $rowData = str_getcsv($row);
+                if (count($rowData) == count($header)) {
+                    $data[] = array_combine($header, $rowData);
+                }
+            }
+            $this->activeFireService->truncateViirsSnppNrt();
+            foreach($data as $value){
+                $this->activeFireService->createViirsSnppNrt($value);
+            }
+        }
+        $responseViirsSnppNrt = Http::get('https://firms.modaps.eosdis.nasa.gov/api/area/csv/5cd8bf082b0eb8dbc40701e0633c65b5/VIIRS_NOAA20_NRT/world/1/'.$currentDate ); //2024-06-01');
+        $csvData = $responseViirsSnppNrt->body();
+        if(!empty($csvData)){
+            $rows = explode("\n", trim($csvData));
+            $header = str_getcsv(array_shift($rows));
+            $data = array();
+
+            foreach ($rows as $row) {
+                $rowData = str_getcsv($row);
+                if (count($rowData) == count($header)) {
+                    $data[] = array_combine($header, $rowData);
+                }
+            }
+            $this->activeFireService->truncateViirsSnppNrt();
+            foreach($data as $value){
+                $this->activeFireService->createViirsSnppNrt($value);
+            }
+        }
     }
 
     public function getActiveFireData(Request $request){
